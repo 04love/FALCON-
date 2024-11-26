b1cb181ea5a9a5e2537d1e17a1f5b231c359728a
04love <167394810+04love@users.noreply.github.com>
2024-11-25 19:43:18 -0700

    app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 import joblib
 
 # Load the dataset
