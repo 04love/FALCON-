804aea66bf451dbb8bd740b71440918da4f59752
04love <167394810+04love@users.noreply.github.com>
2024-12-06 09:17:28 -0700

    app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def exploratory_data_analysis():
     st.title("Exploratory Data Analysis")
 
     # Histograms for numerical columns
-    client_clean['age'].hist(bins=10)
+    merged_cleaned['age'].hist(bins=10)
     plt.title('Age Distribution')
     plt.xlabel('Age')
     plt.ylabel('Frequency')
